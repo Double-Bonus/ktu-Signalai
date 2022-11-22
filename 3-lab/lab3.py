0599bb9f8542c60d2606677d9edf7980aee83247
Zygimantas <zygimantasmarma@gmail.com>
2022-11-23 00:37:59 +0200

    feat: add base of LB-3

--- 3-lab/lab3.py
+++ 3-lab/lab3.py
@@ -2,29 +2,74 @@ import scipy.io
 import matplotlib.pyplot as plt
 import numpy as np
 
+class Adapt:
+    def __init__(self, debug = False):
+        self.debug = debug
+        self.time_s = 22
+        self.Fs_Hz = 8000
+        self.saveDir = "out/"
+         
 
-matData = scipy.io.loadmat('3-lab/lab3_signalai.mat')
+    def drawSignal(self, signal_y, title, show=False):
+            tn = np.linspace(0, self.time_s, num=len(signal_y))
+            plt.figure
+            plt.plot(tn, signal_y, 'r-')
+            plt.title('Signalas laiko srityje, ' + title)
+            plt.xlabel('t, s')
+            plt.ylabel('A')
+            plt.grid(True)
+            plt.savefig(self.saveDir + "amp_" + title,  bbox_inches='tight',
+                        pad_inches=0)
+            if show:
+                plt.show()
+            plt.close()
+
+    # 5 FFT and draw spectrum
+    def drawSpectrum(self, signal_y, title, show= False):
+        nfft = len(signal_y)
+        yf = np.fft.fft(signal_y)
+
+        spectrum = np.abs(yf) / nfft
+        spectrum_db = 20 * np.log10(spectrum/np.max(spectrum))
+
+        k = list(range(0, nfft))
+        f_Hz = [i * (self.Fs_Hz/nfft) for i in k]
+
+        ax = plt.axes()
+        ax.plot(f_Hz, spectrum_db)
+        ax.set_xlim(0, self.Fs_Hz/2)
+        ax.set_ylim(-80, 0)
+        plt.title('Signalas dažnių srityje, ' + title)
+        plt.xlabel('f, Hz')
+        plt.ylabel('S, db')
+        plt.grid(True)
+        plt.savefig(self.saveDir + "spec_" + title,  bbox_inches = 'tight',
+                    pad_inches= 0)
+        if show:
+            plt.show()
+        plt.close()
+
+
+matData = scipy.io.loadmat('signalai/lab3_signalai.mat')
 
 print(matData)
 print(type(matData))
-variklioSig = matData.get('variklioSig')
-kabinosSig = matData.get('kabinosSig')
-pilotoSig = matData.get('pilotoSig')
-
-print(variklioSig)
-print(type(variklioSig))
-print(variklioSig.shape)
-
-print(kabinosSig)
-print(type(kabinosSig))
-
-# plt.plot(kabinosSig, color="red")
-# plt.show()
+variklioSig = matData.get('variklioSig')[0]
+kabinosSig = matData.get('kabinosSig')[0]
+pilotoSig = matData.get('pilotoSig')[0]
 
 
-a = np.zeros(176000)
-print(type(a))
+plt.rcParams.update({'font.family': "Times New Roman"})
+plt.rcParams.update({'font.size': 16})
+plt.rcParams.update({'figure.figsize': (16, 6)}) # horizontally longer figure 
 
-plt.plot(variklioSig)
-plt.show()
+filterObj = Adapt()
+filterObj.drawSignal(variklioSig, "Variklis", True)
+filterObj.drawSignal(kabinosSig, "kabinos", True)
+filterObj.drawSignal(pilotoSig, "piloto", True)
+
+filterObj.drawSpectrum(variklioSig, "VariklisSpekt", True)
+filterObj.drawSpectrum(kabinosSig, "kabinosSpekt", True)
+filterObj.drawSpectrum(pilotoSig, "pilotoSpekt", True)
+
 
